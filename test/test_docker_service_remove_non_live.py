2d29f3915ef1c545c601a0c99aa7f48d789f0f8e
Robert Metcalf <rjmetcal@googlemail.com>
2019-08-05 10:45:32 +0100

    Fixed issue where it shuts down services with simular but different name

--- test/test_docker_service_remove_non_live.py
+++ test/test_docker_service_remove_non_live.py
@@ -13,7 +13,33 @@ scriptName="docker_service_remove_non_live"
 ws_nameGLOB = "testing_ws"
 image_prefix = "metcarob/"
 
+def isServiceThisWS(serviceName, wsName, verDigits):
+  if verDigits == -1:
+    return False
+  if not serviceName.startswith(wsName + "_"):
+    return False
+
+  actualPrefix = ""
+  a = serviceName.split("_")
+  c = len(a)
+  for x in a:
+    c = c - 1
+    if (c+1) > verDigits:
+      actualPrefix += x + "_"
+  if actualPrefix != (wsName + "_"):
+    return False
+
+  return True
+
 class local_helpers(testHelperSuperClass):
+  def _assert_valid_list(self, lis):
+    if len(lis)==0:
+      return
+    expDigits = len(lis[0].split("."))
+    for x in lis:
+      if len(x.split(".")) != expDigits:
+        self.assertFalse(True,msg="Differing number of digits in version list")
+
   def _ensure_image_list_exists(self, lis, ws_name):
     #Now create and delete images
     imgs_to_del = []
@@ -34,6 +60,7 @@ class local_helpers(testHelperSuperClass):
       client.images.remove(image_prefix + ws_name + ":" + ver)
 
   def ensure_list_of_running_service_verisons(self, lis, ws_name=ws_nameGLOB):
+    self._assert_valid_list(lis)
     self._ensure_image_list_exists(lis, ws_name=ws_name)
 
     required_services = {}
@@ -63,17 +90,22 @@ class local_helpers(testHelperSuperClass):
     time.sleep(0.5)
 
   def assertVersionRunningList(self, lis, scriptoutput, ws_name=ws_nameGLOB):
+    self._assert_valid_list(lis)
+    verDigits = -1
+    if len(lis) > 0:
+      verDigits = len(lis[0].split("."))
     required_service_names = []
     for x in lis:
       required_service_names.append(ws_name + "_" + x.replace('.','_'))
 
     running_service_names = []
     for service in client.services.list():
-      if service.name.startswith(ws_name):
+      if isServiceThisWS(service.name, ws_name, verDigits):
         running_service_names.append(service.name)
 
     if not python_Testing_Utilities.objectsEqual(required_service_names,running_service_names):
-      self.printExecuteCommandOutput(scriptoutput)
+      if scriptoutput != None:
+        self.printExecuteCommandOutput(scriptoutput)
       print("Expected services running:" + str(required_service_names))
       print("Actual services running:" + str(running_service_names))
       self.assertTrue(False,msg="Wrong services running")
@@ -86,7 +118,8 @@ class local_helpers(testHelperSuperClass):
         if tag.startswith(image_prefix + ws_name + ":"):
           ver = tag[(len(image_prefix + ws_name) + 1):]
           if ver not in lis:
-            self.printExecuteCommandOutput(scriptoutput)
+            if scriptoutput != None:
+              self.printExecuteCommandOutput(scriptoutput)
             self.assertTrue(False,msg="Image " + tag + " was not removed")
 
 class test_kong_test(local_helpers):
@@ -114,6 +147,7 @@ class test_kong_test(local_helpers):
     expectedOutput += "ws_name:" + ws_nameGLOB + "\n"
     expectedOutput += "version:" + version + "\n"
     expectedOutput += "image_prefix:" + image_prefix + "\n"
+    expectedOutput += "version_digits:3\n"
     expectedOutput += "Error - could not find live service\n"
     expectedOutput += "        (" + ws_nameGLOB + "_" + version.replace('.','_') + ")\n"
     expectedErrorOutput = None
@@ -133,6 +167,7 @@ class test_kong_test(local_helpers):
     expectedOutput += "ws_name:" + ws_nameGLOB + "\n"
     expectedOutput += "version:" + version + "\n"
     expectedOutput += "image_prefix:" + image_prefix + "\n"
+    expectedOutput += "version_digits:3\n"
     expectedOutput += "Error - could not find live service\n"
     expectedOutput += "        (" + ws_nameGLOB + "_" + version.replace('.','_') + ")\n"
     expectedErrorOutput = None
@@ -152,6 +187,7 @@ class test_kong_test(local_helpers):
     expectedOutput += "ws_name:" + ws_nameGLOB + "\n"
     expectedOutput += "version:" + version + "\n"
     expectedOutput += "image_prefix:" + image_prefix + "\n"
+    expectedOutput += "version_digits:3\n"
     expectedOutput += "Check live service is running PASSED (testing_ws_0_4_5)\n"
     expectedOutput += "End of ./scripts/" + scriptName + "\n"
     expectedErrorOutput = None
@@ -170,6 +206,7 @@ class test_kong_test(local_helpers):
     expectedOutput += "ws_name:" + ws_nameGLOB + "\n"
     expectedOutput += "version:" + version + "\n"
     expectedOutput += "image_prefix:" + image_prefix + "\n"
+    expectedOutput += "version_digits:3\n"
     expectedOutput += "Check live service is running PASSED (testing_ws_0_4_5)\n"
     expectedOutput += "Removing old service " + ws_nameGLOB + "_0_4_0" + "\n"
     expectedOutput += "Removing Image " + image_prefix + ws_nameGLOB + ":0.4.0" + "\n"
@@ -190,6 +227,7 @@ class test_kong_test(local_helpers):
     expectedOutput += "ws_name:" + ws_nameGLOB + "\n"
     expectedOutput += "version:" + version + "\n"
     expectedOutput += "image_prefix:" + image_prefix + "\n"
+    expectedOutput += "version_digits:3\n"
     expectedOutput += "Check live service is running PASSED (testing_ws_0_4_5)\n"
     expectedOutput += "Removing old service " + ws_nameGLOB + "_0_3_4" + "\n"
     expectedOutput += "Removing old service " + ws_nameGLOB + "_0_4_0" + "\n"
@@ -202,3 +240,61 @@ class test_kong_test(local_helpers):
     a = self.executeCommand(cmdToExecute, expectedOutput, expectedErrorOutput, [0], 1, False)
 
     self.assertVersionRunningList(["0.4.5"], a)
+
+  def test_stoppingServiceWithShorterNameDosentStopServiceWithLongerName(self):
+    serviceNameToStop = "test_serv"
+    serviceNameShouldNotBeStopped = "test_serv2"
+
+    self.ensure_list_of_running_service_verisons(["0.4.5","0.4.0","0.9.4"], ws_name=serviceNameToStop)
+    self.ensure_list_of_running_service_verisons(["0.4.5","0.4.0","0.9.4"], ws_name=serviceNameShouldNotBeStopped)
+
+    self.assertVersionRunningList(["0.4.5","0.4.0","0.9.4"], scriptoutput=None, ws_name=serviceNameToStop)
+    self.assertVersionRunningList(["0.4.5","0.4.0","0.9.4"], scriptoutput=None, ws_name=serviceNameShouldNotBeStopped)
+
+    expectedOutput = "Start of ./scripts/" + scriptName + "\n"
+    expectedOutput += "Given a service name and version stops all services and removes all images that don't match that version\n"
+    expectedOutput += "ws_name:" + serviceNameToStop + "\n"
+    expectedOutput += "version:" + "0.9.4" + "\n"
+    expectedOutput += "image_prefix:" + image_prefix + "\n"
+    expectedOutput += "version_digits:3\n"
+    expectedOutput += "Check live service is running PASSED (" + serviceNameToStop + "_0_9_4)\n"
+    expectedOutput += "Removing old service " + serviceNameToStop + "_0_4_0" + "\n"
+    expectedOutput += "Removing old service " + serviceNameToStop + "_0_4_5" + "\n"
+    expectedOutput += "Removing Image " + image_prefix + serviceNameToStop + ":0.4.0" + "\n"
+    expectedOutput += "Removing Image " + image_prefix + serviceNameToStop + ":0.4.5" + "\n"
+    expectedOutput += "End of ./scripts/" + scriptName + "\n"
+    expectedErrorOutput = None
+    cmdToExecute = "./scripts/" + scriptName + " " + serviceNameToStop + " " + "0.9.4" + " " + image_prefix
+    a = self.executeCommand(cmdToExecute, expectedOutput, expectedErrorOutput, [0], 1, False)
+
+    self.assertVersionRunningList(["0.9.4"], scriptoutput=a, ws_name=serviceNameToStop)
+    self.assertVersionRunningList(["0.4.5","0.4.0","0.9.4"], scriptoutput=a, ws_name=serviceNameShouldNotBeStopped)
+
+  def test_stoppingServiceWithShorterNameDosentStopServiceWithLongerNameBetterExample(self):
+    serviceNameToStop = "test_serv"
+    serviceNameShouldNotBeStopped = "test_serv_2"
+
+    self.ensure_list_of_running_service_verisons(["0.4.5","0.4.0","0.9.4"], ws_name=serviceNameToStop)
+    self.ensure_list_of_running_service_verisons(["0.4.5","0.4.0","0.9.4"], ws_name=serviceNameShouldNotBeStopped)
+
+    self.assertVersionRunningList(["0.4.5","0.4.0","0.9.4"], scriptoutput=None, ws_name=serviceNameToStop)
+    self.assertVersionRunningList(["0.4.5","0.4.0","0.9.4"], scriptoutput=None, ws_name=serviceNameShouldNotBeStopped)
+
+    expectedOutput = "Start of ./scripts/" + scriptName + "\n"
+    expectedOutput += "Given a service name and version stops all services and removes all images that don't match that version\n"
+    expectedOutput += "ws_name:" + serviceNameToStop + "\n"
+    expectedOutput += "version:" + "0.9.4" + "\n"
+    expectedOutput += "image_prefix:" + image_prefix + "\n"
+    expectedOutput += "version_digits:3\n"
+    expectedOutput += "Check live service is running PASSED (" + serviceNameToStop + "_0_9_4)\n"
+    expectedOutput += "Removing old service " + serviceNameToStop + "_0_4_0" + "\n"
+    expectedOutput += "Removing old service " + serviceNameToStop + "_0_4_5" + "\n"
+    expectedOutput += "Removing Image " + image_prefix + serviceNameToStop + ":0.4.0" + "\n"
+    expectedOutput += "Removing Image " + image_prefix + serviceNameToStop + ":0.4.5" + "\n"
+    expectedOutput += "End of ./scripts/" + scriptName + "\n"
+    expectedErrorOutput = None
+    cmdToExecute = "./scripts/" + scriptName + " " + serviceNameToStop + " " + "0.9.4" + " " + image_prefix
+    a = self.executeCommand(cmdToExecute, expectedOutput, expectedErrorOutput, [0], 1, False)
+
+    self.assertVersionRunningList(["0.9.4"], scriptoutput=a, ws_name=serviceNameToStop)
+    self.assertVersionRunningList(["0.4.5","0.4.0","0.9.4"], scriptoutput=a, ws_name=serviceNameShouldNotBeStopped)
